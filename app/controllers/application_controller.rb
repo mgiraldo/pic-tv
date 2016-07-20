ced5d566fac3b5bbf596496b42ab09e10cd209db
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-07-20 13:22:42 -0400

    :space_invader: updated rails 4 xframe header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Request-Method'] = '*'
+    headers['X-Frame-Options'] = 'ALLOWALL'
   end
 
   def connection_string
